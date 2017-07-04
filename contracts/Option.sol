03120c0e3177ac65d365b8acd37845bc26574e3a
Adam Dossa <adam.dossa@gmail.com>
2017-07-04 23:14:03 +0100

    Tabs to spaces

--- contracts/Option.sol
+++ contracts/Option.sol
@@ -5,83 +5,83 @@ import '../installed_contracts/zeppelin/contracts/token/ERC20.sol';
 //American style vanilla option
 contract Option {
 
-	event OptionEvent(OptionType indexed _optionType, address indexed _issuer, address indexed _counterparty, State _state, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike);
+  event OptionEvent(OptionType indexed _optionType, address indexed _issuer, address indexed _counterparty, State _state, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike);
 
-	enum State { Pending, Live, Active, Exercised, Closed }
+  enum State { Pending, Live, Active, Exercised, Closed }
 
   enum OptionType { Call, Put }
 
-	State public state;
+  State public state;
   OptionType public optionType;
-	ERC20 public token;
-	address public issuer;
-	address public counterparty;
-	uint256 public premium;
-	uint256 public expiry;
-	uint256 public notional;
-	uint256 public strike;
+  ERC20 public token;
+  address public issuer;
+  address public counterparty;
+  uint256 public premium;
+  uint256 public expiry;
+  uint256 public notional;
+  uint256 public strike;
 
-	modifier onlyWhen(State _state) {
-		if (state != _state) {
-			throw;
-		}
-		_;
-	}
+  modifier onlyWhen(State _state) {
+    if (state != _state) {
+      throw;
+    }
+    _;
+  }
 
-	modifier notWhen(State _state) {
-		if (state == _state) {
-			throw;
-		}
-		_;
-	}
+  modifier notWhen(State _state) {
+    if (state == _state) {
+      throw;
+    }
+    _;
+  }
 
-	modifier onlyCounterparty() {
-		if (msg.sender != counterparty) {
-			throw;
-		}
-		_;
-	}
+  modifier onlyCounterparty() {
+    if (msg.sender != counterparty) {
+      throw;
+    }
+    _;
+  }
 
-	modifier onlyIssuer() {
-		if (msg.sender != issuer) {
-			throw;
-		}
-		_;
-	}
+  modifier onlyIssuer() {
+    if (msg.sender != issuer) {
+      throw;
+    }
+    _;
+  }
 
-	function Option(OptionType _optionType, address _tokenAddress, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike) {
-		token = ERC20(_tokenAddress);
+  function Option(OptionType _optionType, address _tokenAddress, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike) {
+    token = ERC20(_tokenAddress);
     optionType = _optionType;
-		issuer = msg.sender;
-		premium = _premium;
-		expiry = _expiry;
-		notional = _notional;
-		strike = _strike;
-		state = State.Pending;
+    issuer = msg.sender;
+    premium = _premium;
+    expiry = _expiry;
+    notional = _notional;
+    strike = _strike;
+    state = State.Pending;
     OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
-	}
+  }
 
-	function destroy() internal onlyWhen(State.Closed) {
-		selfdestruct(issuer);
-	}
+  function destroy() internal onlyWhen(State.Closed) {
+    selfdestruct(issuer);
+  }
 
   function buyOption() payable onlyWhen(State.Live) {
-		if (msg.value != premium) {
-			//Wrong premium being paid
-			throw;
-		}
-		if (block.number > (expiry - 1)) {
-			throw;
-		}
-		counterparty = msg.sender;
-		state = State.Active;
-		OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
-	}
+    if (msg.value != premium) {
+      //Wrong premium being paid
+      throw;
+    }
+    if (block.number > (expiry - 1)) {
+      throw;
+    }
+    counterparty = msg.sender;
+    state = State.Active;
+    OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
+  }
 
   function collateralizeOption() payable onlyWhen(State.Pending);
 
-	function exerciseOption() payable onlyWhen(State.Active);
+  function exerciseOption() payable onlyWhen(State.Active);
 
-	function closeOption();
+  function closeOption();
 
 }
--- contracts/PutOption.sol
+++ contracts/PutOption.sol
@@ -4,71 +4,71 @@ import './Option.sol';
 
 contract PutOption is Option {
 
-	//Option which gives the purchaser the right, but not obligation, to sell <notional> tokens at a <strike> price up until <expiry> block number.
-	function PutOption(OptionType _optionType, address _tokenAddress, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike)
-	Option(_optionType, _tokenAddress, _premium, _expiry, _notional, _strike)
-	{
+  //Option which gives the purchaser the right, but not obligation, to sell <notional> tokens at a <strike> price up until <expiry> block number.
+  function PutOption(OptionType _optionType, address _tokenAddress, uint256 _premium, uint256 _expiry, uint256 _notional, uint256 _strike)
+  Option(_optionType, _tokenAddress, _premium, _expiry, _notional, _strike)
+  {
 
-	}
+  }
 
-	//Collateralizes option with ETH
-	function collateralizeOption() payable onlyIssuer {
+  //Collateralizes option with ETH
+  function collateralizeOption() payable onlyIssuer {
     state = State.Live;
-		//value sent must equal option price to purchase tokens
+    //value sent must equal option price to purchase tokens
     if (msg.value != strike * notional) {
-			throw;
-		}
-		OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
-	}
+      throw;
+    }
+    OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
+  }
 
-	//Exercises option - triggered by counterparty
-	//Counterparty must have allocated an allowance of <notional> to this contract before calling this function
-	function exerciseOption() payable onlyCounterparty {
+  //Exercises option - triggered by counterparty
+  //Counterparty must have allocated an allowance of <notional> to this contract before calling this function
+  function exerciseOption() payable onlyCounterparty {
     state = State.Exercised;
-		//function must be payable since PutOption exercise requires payable
-		//but for a PutOption we don't allow any value to be sent
+    //function must be payable since PutOption exercise requires payable
+    //but for a PutOption we don't allow any value to be sent
     if (msg.value != 0) {
-			throw;
-		}
-		//Don't allow options to be exercised after expiry
+      throw;
+    }
+    //Don't allow options to be exercised after expiry
     if (block.number > expiry) {
-			throw;
-		}
-		//Transfer tokens from counterparty to option contract
+      throw;
+    }
+    //Transfer tokens from counterparty to option contract
     token.transferFrom(counterparty, this, notional);
-		//Transfer price for tokens to counterparty
+    //Transfer price for tokens to counterparty
     if (!msg.sender.send(this.balance)) {
       throw;
     }
-		OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
-	}
+    OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
+  }
 
-	function closeOption() onlyIssuer {
+  function closeOption() onlyIssuer {
 
     state = State.Closed;
 
-		//If the option is Active, it can only be closed after expiry
-		if (state == State.Active && block.number <= expiry) {
-			throw;
-		}
+    //If the option is Active, it can only be closed after expiry
+    if (state == State.Active && block.number <= expiry) {
+      throw;
+    }
 
-		//If contract has been exercised, or is expired, return tokens
-		//NB - expiry is checked above for Active state, so not rechecked here
-		if (state == State.Exercised || state == State.Active) {
-			token.transfer(issuer, notional);
-		}
+    //If contract has been exercised, or is expired, return tokens
+    //NB - expiry is checked above for Active state, so not rechecked here
+    if (state == State.Exercised || state == State.Active) {
+      token.transfer(issuer, notional);
+    }
 
-		//If contract has been collateralized with funds, return them
-		if (state == State.Live) {
-			if (!msg.sender.send(this.balance)) {
-				throw;
-			}
-		}
+    //If contract has been collateralized with funds, return them
+    if (state == State.Live) {
+      if (!msg.sender.send(this.balance)) {
+        throw;
+      }
+    }
 
-		OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
+    OptionEvent(optionType, issuer, counterparty, state, premium, expiry, notional, strike);
 
-		destroy();
+    destroy();
 
-	}
+  }
 
 }
